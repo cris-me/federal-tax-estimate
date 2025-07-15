5a96359eb36e23ffd279df50cf3fd5a2a44c12da
cris-me <melendez.cristopher@live.com>
2025-07-15 15:55:03 -0400

    Adding base endpoint response

--- src/main/java/com/melendez/backendtaxes/BackendTaxesApplication.java
+++ src/main/java/com/melendez/backendtaxes/BackendTaxesApplication.java
@@ -2,6 +2,8 @@ package com.melendez.backendtaxes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class BackendTaxesApplication {
@@ -9,4 +11,12 @@ public class BackendTaxesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendTaxesApplication.class, args);
 	}
+}
+
+@RestController
+class HomeController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello!";
+    }
 }
